67c13540e7549c68e0e65ae8667b64b08274c92a
RiahiF <132400715+RiahiF@users.noreply.github.com>
2023-08-30 19:33:53 +0100

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
-        <p className='orange_gradient font-satoshi font-semibold text-2xl'>.TastyBlend</p>
+        <p className='orange_gradient font-satoshi font-semibold text-2xl'>KitchenFolio</p>
       </Link>
 
       {/* Desktop Navigation */}
