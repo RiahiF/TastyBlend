a84f7fc7bebb2fb1cd63b7da0660919594043ada
RiahiF <132400715+RiahiF@users.noreply.github.com>
2023-09-01 18:21:32 +0200

    Update layout.js
    siteUrl

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 export const metadata = {
   title: "KitchenFolio",
   description: "Discover a world of mouthwatering recipes on KitchenFolio. Join our culinary community to explore, create, and share delicious dishes!",
-  siteUrl: "https://kitchen-folio.vercel.app", // Replace with your site URL
+  siteUrl: "https://kitchen-folio-icav7pkth-riahif.vercel.app", // Replace with your site URL
 };
 
 const RootLayout = ({ children }) => (
